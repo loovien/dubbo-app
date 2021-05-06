4cd0b98066c60f76f708baa2e440a5a3b5671ec9
luowen <loovien@163.com>
2021-05-06 21:39:43 +0800

    xx

--- dubbo-commons/src/test/java/com/example/common/util/TestJwtUtils.java
+++ dubbo-commons/src/test/java/com/example/common/util/TestJwtUtils.java
@@ -1,14 +1,6 @@
 package com.example.common.util;
 
-import com.sun.tracing.dtrace.ArgsAttributes;
-import io.jsonwebtoken.SignatureAlgorithm;
-import io.jsonwebtoken.io.Decoders;
-import io.jsonwebtoken.io.Encoders;
-import io.jsonwebtoken.security.Keys;
 import org.junit.Test;
-
-import javax.crypto.SecretKey;
-import java.util.Base64;
 
 
 /**
@@ -20,10 +12,7 @@ public class TestJwtUtils {
 
     @Test
     public void tokenTest() {
-        String token = JwtUtils.token(11);
-        System.out.println(token);
-        token = "eyJhbGciOiJIUzI1NiJ9.eyJqdGkiOiIxIiwiZXhwIjoxNjIyODc0MTQ1fQ.tlYHXhTKZ3J8UmE8TPocIoJb12cFVZ7SwiDqTXj-WVQ";
-        System.out.println(JwtUtils.getId(token));
+
     }
 
 }
